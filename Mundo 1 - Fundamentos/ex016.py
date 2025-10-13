fe880a84a36e5ed06db4c28ee5a6bd5678da01ab
Taniellen Rodrigues <tanyellens@gmail.com>
2025-10-13 11:01:21 -0300

    Quebrando um número

---
+++ Mundo 1 - Fundamentos/ex016.py
@@ -0,0 +1,5 @@
+#from math import trunc
+num = float(input('Digite um valor: '))
+#print(f'O número {num} tem a parte inteira {trunc(num)}')
+
+print(f'O número {num} tem a parte inteira {int(num)}')
