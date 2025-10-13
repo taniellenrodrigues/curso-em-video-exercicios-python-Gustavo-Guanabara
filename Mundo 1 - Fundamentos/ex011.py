540a70dfa9f5e02b686c16ff361ae238f0b5bee2
Taniellen Rodrigues <tanyellens@gmail.com>
2025-10-13 08:59:06 -0300

    Pintando paredes

---
+++ Mundo 1 - Fundamentos/ex011.py
@@ -0,0 +1,6 @@
+l = float(input('Largura da parede: '))
+a = float(input('Altura da parede: '))
+d = l * a
+li = d / 2
+print(f'Sua parede tem a dimensão de {l} x {a}, e sua área é de {d}m²')
+print(f'Para pintar essa parede, você precisará de {li} L de tinta.')
