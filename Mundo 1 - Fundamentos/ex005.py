6de1de46e6eb05f899a50387bd23210826ef3759
Taniellen Rodrigues <tanyellens@gmail.com>
2025-10-13 08:43:01 -0300

    Antecessor e Sucessor

---
+++ Mundo 1 - Fundamentos/ex005.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número:\n'))
+a = n - 1
+s = n + 1
+print(f'O número é {n}, seu antecessor é {a} e o seu sucessor é {s}')
+#ou podemos fazer desta forma 
+n = int(input('Digite um número:\n'))
+print(f'O número é {n}, seu antecessor é {n-1} e o seu sucessor é {n+1}')
