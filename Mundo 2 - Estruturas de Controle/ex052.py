15bce5a1a10d04d92f291f806ab73d0faa384e3e
Taniellen Rodrigues <tanyellens@gmail.com>
2025-10-13 12:30:32 -0300

    Números primos

---
+++ Mundo 2 - Estruturas de Controle/ex052.py
@@ -0,0 +1,15 @@
+num = int(input('Digite um número: ' \
+''))
+tot = 0
+for c in range(1, num+1):
+    if num % c == 0:
+        print('\033[33m', end=' ')
+        tot = tot + 1
+    else:
+        print('\033[31m', end=' ')
+    print(c, end='-')
+print(f'\n\033[mO número {num} foi divisivél {tot}')
+if tot == 2:
+    print('E por isso ele é primo!!')
+else:
+    print('E por isso ele não é primo')
